38a1c87e619b6b7673302265a0b77c88c5422352
erdemorman90 <erdemorman90@14067deb-acf8-2d61-6e0a-8759f1707fc5>
2012-05-21 18:25:51 +0000

    Returns false for negative numbers too.

--- trunk/src/Base.java
+++ trunk/src/Base.java
@@ -2,7 +2,7 @@ public class Base {
 
   public boolean isPrime(int number){
   
-    if(number ==1) return false;
+    if(number < 2) return false;
     
     for(int i=2; i<number; i++){
       if(number%i==0){
