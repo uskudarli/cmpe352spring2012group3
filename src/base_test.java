d49c11c7db30393e8937c0afced4e33532c5bc89
cigdem.kocberber <cigdem.kocberber@gmail.com>
2012-05-21 16:10:41 +0000

    Test case is created

---
+++ src/base_test.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class base_test {
+
+	@Test
+	public void test() {
+		Base tester = new Base();
+		assertEquals("Result", false, tester.prime(3));
+	}
+
+}
