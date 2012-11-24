e7655dddf46cd6f589b2d1ce677fe927319b93dd
osman.samil327@gmail.com <osman.samil327@gmail.com@14067deb-acf8-2d61-6e0a-8759f1707fc5>
2012-11-24 14:10:54 +0000

    Profile page construction

---
+++ trunk/CmpeCommunityWeb/src/drivers/PostDriver.java
@@ -0,0 +1,38 @@
+package drivers;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import DBPack.DBStatement;
+import Tables.PostsTable;
+
+public class PostDriver {
+
+	public static PostsTable[] getByUserId(int id){
+		try {
+			String query="SELECT * FROM `posts` WHERE `owner_id`=? ORDER BY posting_time desc";
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
+			ps.setInt(1, id);
+			ResultSet result = ps.executeQuery();
+			return convertToArray(result);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+			return new PostsTable[0];
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new PostsTable[0];
+		}
+	}
+	private static PostsTable[] convertToArray(ResultSet result) throws SQLException{
+		int N = 0;
+		while(result.next()) N++;
+		result.beforeFirst();
+		PostsTable[] posts = new PostsTable[N];
+		int i=0;
+		while(result.next())
+			posts[i++] = new PostsTable(result.getInt("id"), result.getInt("owner_id"), result.getString("body"), result.getString("posting_time"));
+		return posts;
+		
+	}
+}
