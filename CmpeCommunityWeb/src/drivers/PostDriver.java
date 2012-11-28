768cb826012f8bb89eb65df8c5a4e09f85216da6
osman.samil327 <osman.samil327@gmail.com>
2012-11-28 18:08:35 +0000

    Wall tab implementation

--- CmpeCommunityWeb/src/drivers/PostDriver.java
+++ CmpeCommunityWeb/src/drivers/PostDriver.java
@@ -8,7 +8,23 @@ import DBPack.DBStatement;
 import Tables.PostsTable;
 
 public class PostDriver {
-
+    
+	public static PostsTable[] getWallPosts(int user_id) {
+		try {
+			String query="(select * from posts where user_id=1) union (select * from posts where id in (select post_id from users_in_posts where user_id=1))";
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
+			ps.setInt(1, user_id);
+			ps.setInt(2, user_id);
+			ResultSet result = ps.executeQuery();
+			return convertToArray(result);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+			return null;
+		}  catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new PostsTable[0];
+		}
+	}
 	public static PostsTable[] getPostsByUserId(int id){
 		try {
 			String query="SELECT * FROM `posts` WHERE `owner_id`=? ORDER BY posting_time desc";
@@ -50,6 +66,21 @@ public class PostDriver {
 		while(result.next())
 			posts[i++] = new PostsTable(result.getInt("id"), result.getInt("owner_id"), result.getString("body"), result.getString("posting_time"));
 		return posts;
-		
+	}
+	public static boolean addPosts(int user_id,String post) {
+		try{
+			String query="INSERT INTO posts (owner_id,body,posting_time) VALUES (?,?,NOW())" ;	
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
+			ps.setInt(1, user_id);
+			ps.setString(2,post);
+			ps.executeUpdate();
+		  return true; // means false
+		}  catch(SQLException e) {
+			return false;
+		} catch (Exception e) {
+			return false;
+		} finally {
+			
+		}
 	}
 }
