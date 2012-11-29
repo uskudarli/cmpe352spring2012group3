212e2b800da507ee460ca98a4c3ec3b22c4b32a1
emresunecli <emresunecli@gmail.com>
2012-11-29 01:42:40 +0000

    Fixed posting user's name in profile wall.

--- CmpeCommunityWeb/src/Servlets/Posts.java
+++ CmpeCommunityWeb/src/Servlets/Posts.java
@@ -7,17 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import drivers.PostDriver;
-import drivers.TagsDriver;
-
-import Tables.PostsTable;
-import Tables.TagsTable;
-import Tables.UserTable;
 
 public class Posts extends ServletBase {
 
 	public Posts(HttpServletRequest request, HttpServletResponse response) {
 		super(request, response);
 	}
+
 	public void addPosts(Integer user_id) throws ServletException, IOException{
 		String post =  request.getParameter("posts");
 		PostDriver.addPosts(user_id,post);
