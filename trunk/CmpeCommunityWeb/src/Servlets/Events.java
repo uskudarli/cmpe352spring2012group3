8e768b754f3f5b1e6c4825cbd46ad78bb08e4c15
cigdem.kocberber@gmail.com <cigdem.kocberber@gmail.com@14067deb-acf8-2d61-6e0a-8759f1707fc5>
2012-12-25 17:35:03 +0000

    Events.java is created with myEvents and attendedEvents functions

---
+++ trunk/CmpeCommunityWeb/src/Servlets/Events.java
@@ -0,0 +1,52 @@
+package Servlets;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Tables.PostsTable;
+
+import Tables.SurveyTable;
+
+import Tables.ReplyTable;
+
+import Tables.TagsTable;
+import Tables.UserTable;
+import drivers.PostDriver;
+import drivers.ReplyDriver;
+import drivers.SurveyDriver;
+import drivers.TagsDriver;
+import drivers.UserDriver;
+
+
+public class Events extends ServletBase{
+	
+	public Events(HttpServletRequest request, HttpServletResponse response) {
+		super(request, response);
+	}
+	
+	public void myEvents(Integer userId)throws IOException, ServletException{
+		
+		if(getCurrentUser() == null)
+			return;
+
+		request.setAttribute("type", "myEvents");
+		
+		request.getRequestDispatcher("/EventList.jsp").include(request, response);
+		
+	}
+	
+	public void attendedEvents(Integer userId)throws IOException, ServletException{
+		if(getCurrentUser() == null)
+			return;
+		
+		request.getRequestDispatcher("/EventList.jsp").include(request, response);
+		
+	}
+
+}
+
