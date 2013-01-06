9b7d4db8d007eaa366ad128e11fad687082b10be
osman.samil327@gmail.com <osman.samil327@gmail.com@14067deb-acf8-2d61-6e0a-8759f1707fc5>
2013-01-06 22:10:45 +0000

    Event implementation

--- trunk/CmpeCommunityWeb/src/Servlets/Events.java
+++ trunk/CmpeCommunityWeb/src/Servlets/Events.java
@@ -8,19 +8,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import Tables.PostsTable;
-
-import Tables.SurveyTable;
-
-import Tables.ReplyTable;
-
-import Tables.TagsTable;
-import Tables.UserTable;
-import drivers.PostDriver;
-import drivers.ReplyDriver;
-import drivers.SurveyDriver;
-import drivers.TagsDriver;
-import drivers.UserDriver;
+import drivers.EventDriver;
+import Tables.EventTable;
 
 
 public class Events extends ServletBase{
@@ -30,14 +19,20 @@ public class Events extends ServletBase{
 	}
 	
 	public void myEvents(Integer userId)throws IOException, ServletException{
+		
 		if(getCurrentUser() == null){
 			request.getRequestDispatcher("/User/login").forward(request, response);
+
 			return;
 		}
 
 		request.setAttribute("type", "myEvents");
+
+		EventTable[] events=EventDriver.getUserEvent(userId);
+
 		request.setAttribute("user",getCurrentUser());
-		request.getRequestDispatcher("/EventList.jsp").include(request, response);
+        request.setAttribute("events", events);
+        request.getRequestDispatcher("/EventList.jsp").include(request, response);
 	}
 	
 	public void attendedEvents(Integer userId)throws IOException, ServletException{
@@ -48,7 +43,22 @@ public class Events extends ServletBase{
 		
 		request.setAttribute("type", "");
 		request.setAttribute("user",getCurrentUser());
-
+        EventTable[] events=EventDriver.getUserJoinedEvent(userId);
+        request.setAttribute("events", events);
+		request.getRequestDispatcher("/EventList.jsp").include(request, response);
+		
+	}
+	public void addEvents(Integer userId)throws IOException, ServletException{
+		if(getCurrentUser() == null)
+			return;
+		
+		request.setAttribute("type", "");
+		
+		String time=request.getParameter("datetime");
+		String description=request.getParameter("description");
+		String place=request.getParameter("place");
+		java.sql.Timestamp eventTime=java.sql.Timestamp.valueOf(time);
+		EventDriver.createEvent(userId, description, place, eventTime);
 		request.getRequestDispatcher("/EventList.jsp").include(request, response);
 		
 	}
