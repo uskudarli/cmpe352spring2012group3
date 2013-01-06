460a9ce203d3238f1548bc244ba6c339c21b9e1d
osman.samil327@gmail.com <osman.samil327@gmail.com@14067deb-acf8-2d61-6e0a-8759f1707fc5>
2013-01-06 22:23:44 +0000

    Event implementation

---
+++ trunk/CmpeCommunityWeb/src/drivers/EventDriver.java
@@ -0,0 +1,104 @@
+package drivers;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Statement;
+import com.sun.jmx.snmp.Timestamp;
+
+import DBPack.DBStatement;
+import Tables.ChoiceTable;
+import Tables.PostsTable;
+import Tables.EventTable;
+import Tables.TagsTable;
+
+public class EventDriver {
+	public static EventTable[] getUserEvent(int userId) {
+		try {
+			String query="SELECT * FROM `events` WHERE `user_id`=? order by creation_time";
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
+			ps.setInt(1, userId);
+			ResultSet result = ps.executeQuery();
+			return convertEventToArray(result);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+			return new EventTable[0];
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new EventTable[0];
+		}
+	}
+	public static EventTable[] getUserJoinedEvent(int userId) {
+		try{
+			String query="SELECT `events`.* FROM `events` INNER JOIN `users_in_Event` ON `users_in_Event`.`user_id`=`events`.`user_id` WHERE `events`.`user_id`= ? order by creation_time" ;
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
+			ps.setInt(1, userId);
+			ResultSet set = ps.executeQuery();
+			return convertEventToArray(set);
+		}  catch(SQLException e) {
+			System.out.println(e.getMessage());
+			return new EventTable[0];
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new EventTable[0];
+		}
+	}
+	public static boolean createEvent(int userId,String description,String place,java.sql.Timestamp eventTime) {
+		try{
+			String query="INSERT INTO events (user_id,place,event_time,description,creation_time) VALUES (?,?,?,?,NOW())" ;	
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			ps.setInt(1, userId);
+			ps.setString(2, place);
+			ps.setTimestamp(3,eventTime);
+			ps.setString(4, description);
+			ps.executeUpdate();
+			return true;
+		}  catch(SQLException e) {
+			return false;
+		} catch (Exception e) {
+			return false;
+		}
+		
+	}
+public static EventTable getEvent(int EventId) {
+	try {
+		String query="SELECT * FROM `events` WHERE `id`=?";
+		PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
+		ps.setInt(1, EventId);
+		ResultSet result = ps.executeQuery();
+		return new EventTable(result.getInt("id"),result.getInt("user_id"),result.getString("description"),result.getString("place"),result.getTimestamp("event_time"));
+	} catch(SQLException e) {
+		System.out.println(e.getMessage());
+		return null;
+	} catch (Exception e) {
+		System.out.println(e.getMessage());
+		return null;
+	}
+}
+private static EventTable[] convertEventToArray(ResultSet result) throws SQLException{
+	int N = 0;
+	while(result.next()) N++;
+	result.beforeFirst();
+	EventTable[] events = new EventTable[N];
+	int i=0;
+	while(result.next())
+		events[i++] = new EventTable(result.getInt("id"),result.getInt("user_id"),result.getString("description"),result.getString("place"),result.getTimestamp("event_time"));
+	return events;
+}
+public static boolean insertUsersInEvent(int userId,int EventId) {
+	try{
+		String query="INSERT INTO users_in_Event (Event_id,user_id) VALUES (?,?)" ;	
+		PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+		ps.setInt(1, EventId);
+		ps.setInt(2, userId);
+		ps.executeUpdate();
+		return true;
+	}  catch(SQLException e) {
+		return false;
+	} catch (Exception e) {
+		return false;
+	}
+	
+}
+}
