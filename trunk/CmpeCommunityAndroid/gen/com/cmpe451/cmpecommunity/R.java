6533ae04dc5f8fb19e911df9b577ab74b8f50df4
alpynaq@gmail.com <alpynaq@gmail.com@14067deb-acf8-2d61-6e0a-8759f1707fc5>
2013-01-08 20:13:29 +0000

    Post to user functionality is added.

--- trunk/CmpeCommunityAndroid/gen/com/cmpe451/cmpecommunity/R.java
+++ trunk/CmpeCommunityAndroid/gen/com/cmpe451/cmpecommunity/R.java
@@ -21,28 +21,29 @@ public final class R {
         public static final int tags=0x7f020007;
     }
     public static final class id {
-        public static final int btnLogin=0x7f06000a;
-        public static final int btnRegister=0x7f060011;
+        public static final int btnLogin=0x7f06000b;
+        public static final int btnRegister=0x7f060012;
         public static final int content=0x7f060002;
-        public static final int email=0x7f060008;
-        public static final int events=0x7f060005;
-        public static final int footer=0x7f060007;
-        public static final int header=0x7f060006;
-        public static final int home=0x7f060014;
-        public static final int link_to_login=0x7f060012;
-        public static final int link_to_register=0x7f06000b;
-        public static final int logout=0x7f060015;
+        public static final int email=0x7f060009;
+        public static final int events=0x7f060006;
+        public static final int footer=0x7f060008;
+        public static final int header=0x7f060007;
+        public static final int home=0x7f060015;
+        public static final int link_to_login=0x7f060013;
+        public static final int link_to_register=0x7f06000c;
+        public static final int logout=0x7f060016;
         public static final int name=0x7f060000;
-        public static final int password=0x7f060009;
+        public static final int password=0x7f06000a;
+        public static final int postText=0x7f060005;
         public static final int posting_time=0x7f060001;
-        public static final int reg_email=0x7f06000e;
-        public static final int reg_lastname=0x7f06000d;
-        public static final int reg_name=0x7f06000c;
-        public static final int reg_password=0x7f06000f;
-        public static final int reg_passwordrepeat=0x7f060010;
+        public static final int reg_email=0x7f06000f;
+        public static final int reg_lastname=0x7f06000e;
+        public static final int reg_name=0x7f06000d;
+        public static final int reg_password=0x7f060010;
+        public static final int reg_passwordrepeat=0x7f060011;
         public static final int replyCount=0x7f060003;
         public static final int replyText=0x7f060004;
-        public static final int tag=0x7f060013;
+        public static final int tag=0x7f060014;
     }
     public static final class layout {
         public static final int feed_item=0x7f030000;
--- trunk/CmpeCommunityAndroid/src/com/cmpe451/cmpecommunity/FeedActivity.java
+++ trunk/CmpeCommunityAndroid/src/com/cmpe451/cmpecommunity/FeedActivity.java
@@ -2,29 +2,41 @@ package com.cmpe451.cmpecommunity;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
 public class FeedActivity extends ListActivity{
 	private FeedAdapter adapter;
-
+	
+	String path;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,14 +44,34 @@ public class FeedActivity extends ListActivity{
 
 		adapter = new FeedAdapter(this, R.layout.feed_item);
 
-		String path = getIntent().getExtras().getString("FeedType");
+		path = getIntent().getExtras().getString("FeedType");
+		if(path.equalsIgnoreCase("wall"))
+		{
+			EditText postText = (EditText) findViewById(R.id.postText);
+			postText.setVisibility(View.VISIBLE);
+			if(StaticUser.chosenUser.getId() != StaticUser.currentUser.getId())
+				postText.setHint("@" + StaticUser.chosenUser.getName());
+		}
+			
 		if(getIntent().getExtras().getBoolean("TagPage")) 
 			path += "/" + StaticUser.chosenTag.getId();
-		else 
+		else
 			path += "/" + StaticUser.chosenUser.getId(); 
 		
-		System.out.println(path);
+		final EditText postText = (EditText) findViewById(R.id.postText);
+		postText.setOnEditorActionListener(new OnEditorActionListener() {
+		    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		        if (actionId == EditorInfo.IME_ACTION_SEND) {
+		        	new PostTask().execute(postText.getText().toString());
+		        	((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+		            return false;
+		        }
+		        return false;
+		    }
+		});
+		
 		new HttpTask().execute(path);
+		
 	}
 
 	@Override
@@ -132,4 +164,81 @@ public class FeedActivity extends ListActivity{
 		}
 	}
 
+	final class PostTask extends AsyncTask<String, Boolean, String> {
+		private String post;
+		
+		@Override
+		protected String doInBackground(String... param) {
+		    publishProgress(true);
+
+		    post = param[0];
+		    
+			// Creating HTTP client
+			HttpClient httpClient = new DefaultHttpClient();
+
+			// Creating HTTP Post
+			HttpPost httpPost = new HttpPost(StaticUser.URL + "/CmpeCommunityWeb/AndroidApi/postToUser/" + StaticUser.chosenUser.getId());
+
+			// Url Encoding the POST parameters
+			try {
+				List<NameValuePair> nameValuePair = StaticUser.GetNameValuePair();
+				nameValuePair.add(new BasicNameValuePair("body", post));
+				httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+
+				// Making HTTP Request
+				HttpResponse response = httpClient.execute(httpPost);
+
+				return EntityUtils.toString(response.getEntity());
+
+			} catch (ClientProtocolException e) {
+				// writing exception to log
+				e.printStackTrace();
+
+			} catch (IOException e) {
+				// writing exception to log
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+
+		@Override
+		protected void onProgressUpdate(Boolean... progress) {
+			FeedActivity.this.setProgressBarIndeterminateVisibility(progress[0]);
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			publishProgress(false);
+
+			if(result == null)
+			{
+				Toast.makeText(FeedActivity.this, "Server connection error!", Toast.LENGTH_SHORT).show();
+				return;
+			}
+
+			try
+			{
+				JSONObject json = new JSONObject(result);
+
+				if(json.getBoolean("success"))
+				{
+					Feed feed = new Feed(0, StaticUser.currentUser.getName(), StaticUser.currentUser.getId(), post, "now");
+					feed.setReplies(new ArrayList<Reply>());
+					adapter.insert(feed, 0);
+					
+					EditText postText = (EditText) findViewById(R.id.postText);
+					postText.setText("");
+				}
+				else
+				{
+					Toast.makeText(FeedActivity.this, json.getString("error"), Toast.LENGTH_SHORT).show();
+				}
+			} catch (JSONException e) {
+				// writing exception to log
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
