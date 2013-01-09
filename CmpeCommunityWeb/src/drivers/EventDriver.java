346da22c368481a8b55836d72c542c443119f7bd
emresunecli <emresunecli@gmail.com>
2013-01-09 00:43:00 +0000

    changed timestamp to string and some bug fixes

--- CmpeCommunityWeb/src/drivers/EventDriver.java
+++ CmpeCommunityWeb/src/drivers/EventDriver.java
@@ -4,14 +4,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.mysql.jdbc.Statement;
-import com.sun.jmx.snmp.Timestamp;
-
 import DBPack.DBStatement;
-import Tables.ChoiceTable;
-import Tables.PostsTable;
 import Tables.EventTable;
-import Tables.TagsTable;
+
+import com.mysql.jdbc.Statement;
 
 public class EventDriver {
 	public static EventTable[] getUserEvent(int userId) {
@@ -31,7 +27,7 @@ public class EventDriver {
 	}
 	public static EventTable[] getUserJoinedEvent(int userId) {
 		try{
-			String query="SELECT `events`.* FROM `events` INNER JOIN `users_in_Event` ON `users_in_Event`.`user_id`=`events`.`user_id` WHERE `events`.`user_id`= ? order by creation_time" ;
+			String query="SELECT `events`.* FROM `events` INNER JOIN `users_in_event` ON `users_in_event`.`user_id`=`events`.`user_id` WHERE `events`.`user_id`= ? order by creation_time" ;
 			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
 			ps.setInt(1, userId);
 			ResultSet set = ps.executeQuery();
@@ -44,14 +40,56 @@ public class EventDriver {
 			return new EventTable[0];
 		}
 	}
-	public static boolean createEvent(int userId,String description,String place,java.sql.Timestamp eventTime) {
+	public static boolean createEvent(int userId,String description,String place,String eventTime) {
 		try{
-			String query="INSERT INTO events (user_id,place,event_time,description,creation_time) VALUES (?,?,?,?,NOW())" ;	
+			String query="INSERT INTO events (user_id, place, event_time, description, creation_time) VALUES (?,?,?,?,NOW())" ;	
 			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
 			ps.setInt(1, userId);
 			ps.setString(2, place);
-			ps.setTimestamp(3,eventTime);
+			ps.setString(3,eventTime);
 			ps.setString(4, description);
+			ps.executeUpdate();
+			return true;
+		}  catch(SQLException e) {
+			System.out.println(e.getMessage());
+			return false;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+
+	}
+	public static EventTable getEvent(int EventId) {
+		try {
+			String query="SELECT * FROM `events` WHERE `id`=?";
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
+			ps.setInt(1, EventId);
+			ResultSet result = ps.executeQuery();
+			return new EventTable(result.getInt("id"),result.getInt("user_id"),result.getString("description"),result.getString("place"),result.getString("event_time"));
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+			return null;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+	private static EventTable[] convertEventToArray(ResultSet result) throws SQLException{
+		int N = 0;
+		while(result.next()) N++;
+		result.beforeFirst();
+		EventTable[] events = new EventTable[N];
+		int i=0;
+		while(result.next())
+			events[i++] = new EventTable(result.getInt("id"),result.getInt("user_id"),result.getString("description"),result.getString("place"),result.getString("event_time"));
+		return events;
+	}
+	public static boolean insertUsersInEvent(int userId,int EventId) {
+		try{
+			String query="INSERT INTO users_in_Event (Event_id,user_id) VALUES (?,?)" ;	
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			ps.setInt(1, EventId);
+			ps.setInt(2, userId);
 			ps.executeUpdate();
 			return true;
 		}  catch(SQLException e) {
@@ -59,46 +97,6 @@ public class EventDriver {
 		} catch (Exception e) {
 			return false;
 		}
-		
+
 	}
-public static EventTable getEvent(int EventId) {
-	try {
-		String query="SELECT * FROM `events` WHERE `id`=?";
-		PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
-		ps.setInt(1, EventId);
-		ResultSet result = ps.executeQuery();
-		return new EventTable(result.getInt("id"),result.getInt("user_id"),result.getString("description"),result.getString("place"),result.getTimestamp("event_time"));
-	} catch(SQLException e) {
-		System.out.println(e.getMessage());
-		return null;
-	} catch (Exception e) {
-		System.out.println(e.getMessage());
-		return null;
-	}
-}
-private static EventTable[] convertEventToArray(ResultSet result) throws SQLException{
-	int N = 0;
-	while(result.next()) N++;
-	result.beforeFirst();
-	EventTable[] events = new EventTable[N];
-	int i=0;
-	while(result.next())
-		events[i++] = new EventTable(result.getInt("id"),result.getInt("user_id"),result.getString("description"),result.getString("place"),result.getTimestamp("event_time"));
-	return events;
-}
-public static boolean insertUsersInEvent(int userId,int EventId) {
-	try{
-		String query="INSERT INTO users_in_Event (Event_id,user_id) VALUES (?,?)" ;	
-		PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-		ps.setInt(1, EventId);
-		ps.setInt(2, userId);
-		ps.executeUpdate();
-		return true;
-	}  catch(SQLException e) {
-		return false;
-	} catch (Exception e) {
-		return false;
-	}
-	
-}
 }
