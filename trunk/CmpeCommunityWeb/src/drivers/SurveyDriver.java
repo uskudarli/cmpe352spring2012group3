0fd00d5b014c64e3702696b84d31f995399622f9
emresunecli@gmail.com <emresunecli@gmail.com@14067deb-acf8-2d61-6e0a-8759f1707fc5>
2013-01-09 00:43:14 +0000

    some fixes and new functionalities

--- trunk/CmpeCommunityWeb/src/drivers/SurveyDriver.java
+++ trunk/CmpeCommunityWeb/src/drivers/SurveyDriver.java
@@ -44,6 +44,21 @@ public class SurveyDriver {
 			return new SurveyTable[0];
 		}
 	}
+	public static int[] getUserJoinedSurveyIds(int userId){
+		try{
+			String query="SELECT `surveys`.* FROM `surveys` INNER JOIN `users_in_survey` ON `users_in_survey`.`user_id`=`surveys`.`user_id` WHERE `surveys`.`user_id`= ? order by creation_time" ;
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
+			ps.setInt(1, userId);
+			ResultSet set = ps.executeQuery();
+			return convertIntToArray(set);
+		}  catch(SQLException e) {
+			System.out.println(e.getMessage());
+			return new int[0];
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new int[0];
+		}
+	}
 	public static boolean createSurvey(int userId,String question,String[] choices) {
 		try{
 			String query="INSERT INTO surveys (question,user_id,creation_time) VALUES (?,?,NOW())" ;	
@@ -69,140 +84,150 @@ public class SurveyDriver {
 		} catch (Exception e) {
 			return false;
 		}
-		
+
 	}
 
-public static boolean voteChoice(int choiceId) {
-	try{
-		String query="UPDATE choices set votes=votes+1 where id=?" ;	
-		PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-		ps.setInt(1, choiceId);
-		ps.executeUpdate();
-		return true;
-	}  catch(SQLException e) {
-		return false;
-	} catch (Exception e) {
-		return false;
-	}
-}
-public static SurveyTable getSurvey(int surveyId) {
-	try {
-		String query="SELECT * FROM `surveys` WHERE `id`=?";
-		PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
-		ps.setInt(1, surveyId);
-		ResultSet result = ps.executeQuery();
-		return new SurveyTable(result.getInt("id"),result.getInt("user_id"),result.getString("question"),getChoices(surveyId));
-	} catch(SQLException e) {
-		System.out.println(e.getMessage());
-		return null;
-	} catch (Exception e) {
-		System.out.println(e.getMessage());
-		return null;
-	}
-}
-public static ChoiceTable[] getChoices(int surveyId) {
-	try {
-		String query="SELECT * FROM `choices` WHERE `survey_id`=?";
-		PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
-		ps.setInt(1, surveyId);
-		ResultSet result = ps.executeQuery();
-		return convertToArray(result);
-	} catch(SQLException e) {
-		System.out.println(e.getMessage());
-		return new ChoiceTable[0];
-	} catch (Exception e) {
-		System.out.println(e.getMessage());
-		return new ChoiceTable[0];
-	}
-}
-private static ChoiceTable[] convertToArray(ResultSet result) throws SQLException{
-	int N = 0;
-	int totalVotes=0;
-	while(result.next()) { 
-		N++;
-	    totalVotes+=result.getInt("votes");
-	}
-	result.beforeFirst();
-	ChoiceTable[] choices = new ChoiceTable[N];
-	int i=0;
-	while(result.next())
-		choices[i++] = new ChoiceTable(result.getInt("id"), result.getInt("survey_id"), result.getString("choice"), result.getInt("votes"),(totalVotes==0?0:result.getInt("votes")/totalVotes)*100);
-	return choices;
-}
-private static SurveyTable[] convertSurveyToArray(ResultSet result) throws SQLException{
-	int N = 0;
-	while(result.next()) N++;
-	result.beforeFirst();
-	SurveyTable[] surveys = new SurveyTable[N];
-	int i=0;
-	while(result.next())
-		surveys[i++] = new SurveyTable(result.getInt("id"),result.getInt("user_id"),result.getString("question"),getChoices(result.getInt("id")));
-	return surveys;
-}
-public static boolean insertUsersInSurvey(int userId,int surveyId) {
-	try{
-		String query="INSERT INTO users_in_survey (survey_id,user_id) VALUES (?,?)" ;	
-		PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-		ps.setInt(1, surveyId);
-		ps.setInt(2, userId);
-		ps.executeUpdate();
-		return true;
-	}  catch(SQLException e) {
-		return false;
-	} catch (Exception e) {
-		return false;
-	}
-	
-}
-private static boolean insertChoices(int surveyId, String[] choices) {
-	try{
-		for (int i=0;i<choices.length;i++) {
-			
-			if (!addChoice(surveyId,choices[i]))
-				return false;
-			
+	public static boolean voteChoice(int choiceId) {
+		try{
+			String query="UPDATE choices set votes=votes+1 where id=?" ;	
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			ps.setInt(1, choiceId);
+			ps.executeUpdate();
+			return true;
+		}  catch(SQLException e) {
+			return false;
+		} catch (Exception e) {
+			return false;
 		}
-		return true;
-	} catch (Exception e) {
-		return false;
 	}
-}
-private static boolean addChoice(int surveyId, String choice) {
-	try{
-		String query="INSERT INTO choices (survey_id,choice) VALUES (?,?)" ;	
-		PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-		ps.setInt(1, surveyId);
-		ps.setString(2, choice);
-		ps.executeUpdate();
-		return true;
-	}  catch(SQLException e) {
-		return false;
-	} catch (Exception e) {
-		return false;
-	}
-}
-public static ArrayList<Double> getVotes(int surveyId) {
-	ArrayList<Double> list=new ArrayList<Double>();
-	double totalVotes=0;
-	try {
-		String query="SELECT * FROM `surveys` WHERE `id`=?";
-		PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
-		ps.setInt(1, surveyId);
-		ResultSet result = ps.executeQuery();
-		while (result.next()) {
-			list.add(result.getInt("votes")/1.0);
-			totalVotes+=result.getInt("votes");			
+	public static SurveyTable getSurvey(int surveyId) {
+		try {
+			String query="SELECT * FROM `surveys` WHERE `id`=?";
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
+			ps.setInt(1, surveyId);
+			ResultSet result = ps.executeQuery();
+			return new SurveyTable(result.getInt("id"),result.getInt("user_id"),result.getString("question"),getChoices(surveyId));
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+			return null;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return null;
 		}
-		for (int i=0;i<list.size();i++) {
-			list.set(i,list.get(i)*100/totalVotes);
-		}
-		return list;
-	} catch (Exception e) {
-		System.out.println(e.getMessage());
-		return null;
 	}
-	
-	
-}
+	public static ChoiceTable[] getChoices(int surveyId) {
+		try {
+			String query="SELECT * FROM `choices` WHERE `survey_id`=?";
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
+			ps.setInt(1, surveyId);
+			ResultSet result = ps.executeQuery();
+			return convertToArray(result);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+			return new ChoiceTable[0];
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new ChoiceTable[0];
+		}
+	}
+	private static ChoiceTable[] convertToArray(ResultSet result) throws SQLException{
+		int N = 0;
+		int totalVotes=0;
+		while(result.next()) { 
+			N++;
+			totalVotes+=result.getInt("votes");
+		}
+		result.beforeFirst();
+		ChoiceTable[] choices = new ChoiceTable[N];
+		int i=0;
+		while(result.next())
+			choices[i++] = new ChoiceTable(result.getInt("id"), result.getInt("survey_id"), result.getString("choice"), result.getInt("votes"),(totalVotes==0?0:result.getInt("votes")/totalVotes)*100);
+		return choices;
+	}
+	private static int[] convertIntToArray(ResultSet result) throws SQLException{
+		int N = 0;
+		while(result.next()) N++;
+		result.beforeFirst();
+		int[] surveys = new int[N];
+		int i=0;
+		while(result.next())
+			surveys[i++] = result.getInt("id");
+		return surveys;
+	}
+	private static SurveyTable[] convertSurveyToArray(ResultSet result) throws SQLException{
+		int N = 0;
+		while(result.next()) N++;
+		result.beforeFirst();
+		SurveyTable[] surveys = new SurveyTable[N];
+		int i=0;
+		while(result.next())
+			surveys[i++] = new SurveyTable(result.getInt("id"),result.getInt("user_id"),result.getString("question"),getChoices(result.getInt("id")));
+		return surveys;
+	}
+	public static boolean insertUsersInSurvey(int userId,int surveyId) {
+		try{
+			String query="INSERT INTO users_in_survey (survey_id,user_id) VALUES (?,?)" ;	
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			ps.setInt(1, surveyId);
+			ps.setInt(2, userId);
+			ps.executeUpdate();
+			return true;
+		}  catch(SQLException e) {
+			return false;
+		} catch (Exception e) {
+			return false;
+		}
+
+	}
+	private static boolean insertChoices(int surveyId, String[] choices) {
+		try{
+			for (int i=0;i<choices.length;i++) {
+
+				if (!addChoice(surveyId,choices[i]))
+					return false;
+
+			}
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	private static boolean addChoice(int surveyId, String choice) {
+		try{
+			String query="INSERT INTO choices (survey_id,choice) VALUES (?,?)" ;	
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			ps.setInt(1, surveyId);
+			ps.setString(2, choice);
+			ps.executeUpdate();
+			return true;
+		}  catch(SQLException e) {
+			return false;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	public static ArrayList<Double> getVotes(int surveyId) {
+		ArrayList<Double> list=new ArrayList<Double>();
+		double totalVotes=0;
+		try {
+			String query="SELECT * FROM `surveys` WHERE `id`=?";
+			PreparedStatement ps=(PreparedStatement) DBStatement.getMainConnection().prepareStatement(query);
+			ps.setInt(1, surveyId);
+			ResultSet result = ps.executeQuery();
+			while (result.next()) {
+				list.add(result.getInt("votes")/1.0);
+				totalVotes+=result.getInt("votes");			
+			}
+			for (int i=0;i<list.size();i++) {
+				list.set(i,list.get(i)*100/totalVotes);
+			}
+			return list;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+
+
+	}
 
 }
